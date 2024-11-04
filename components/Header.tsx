92597f6ecf8c85fe5e6a27ec4ee66114a846222d
ShimHyesu <hesushim@gmail.com>
2024-11-04 11:49:04 +0900

    Setting: Header back button 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,34 +3,47 @@
 //
 import React from "react";
 
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
+
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 import { colors } from "@/styles/globalColors";
 
 const LOGO = require("@/assets/images/crayon.png");
 
-//기본적인 헤더
-export const BasicHeader = () => {
-
-    return (
-        <View style={styles.container}>
-            <Image source={LOGO} style={styles.image} />
-        </View>
-
-    );
+interface BasicHeaderProps {
+  back?: boolean;
+  backPress?: () => void;
 }
 
+//기본적인 헤더
+export const BasicHeader = ({ back, backPress }: BasicHeaderProps) => {
+  return (
+    <View style={[styles.container, {justifyContent: back? 'space-between' : 'center'}]}>
+      {back && (
+        <TouchableOpacity onPress={backPress}>
+          <MaterialCommunityIcons name="backburger" size={50} color="black" />
+        </TouchableOpacity>
+      )}
+      <Image source={LOGO} style={styles.image} />
+      {back && <View style={{ width: 50 }} />}
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        height: 80,
-        backgroundColor: colors.beige,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    image:{
-        width:65,
-        height:70,
-        resizeMode:'contain'
-    }
+  container: {
+    width: "100%",
+    height: 80,
+    backgroundColor: colors.beige,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10
+  },
+  image: {
+    width: 65,
+    height: 70,
+    resizeMode: "contain",
+  },
 });
